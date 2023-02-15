310316e0edb893e9a7b014c4aad8d7f2132c5c61
Kadir Hali <kadirhali07@gmail.com>
2023-02-15 16:41:38 +0300

    15.02.2023

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -18,10 +18,47 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        public void Add(Car car)
+        {
+            if (car.Description.Length>=2 && car.DailyPrice>0)
+            {
+                _carDal.Add(car);
+            }
+            else
+            {
+                Console.WriteLine("Kurallara uymayan bir araba ekleyemezsiniz!");
+            }
+        }
+
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
+        }
+
         public List<Car> GetAll()
         {
             // İş Kodları
             return _carDal.GetAll();    
+        }
+
+        public List<Car> GetByDailyPrice(decimal min, decimal max)
+        {
+            return _carDal.GetAll(c => c.DailyPrice>=min && c.DailyPrice<=max);
+        }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(c => c.BrandId== id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(c => c.ColorId == id);
+        }
+
+        public void Update(Car car)
+        {
+            _carDal.Update(car);
         }
     }
 }
--- Console/Program.cs
+++ Console/Program.cs
@@ -1,9 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 
-CarManager carManager = new CarManager(new InMemoryCarDal());
+CarManager carManager = new CarManager(new EfCarDal());
+
+//Car car1 = new Car {Id=1,ColorId=1,BrandId=1,Description="Opel Astra",ModelYear=2016,DailyPrice=750 };
+//carManager.Add(car1);
 
 foreach (var car in carManager.GetAll())
 {
