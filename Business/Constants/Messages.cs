a9ca8156efde94ea1efb2c717a22dc8bb5354310
Kadir Hali <kadirhali07@gmail.com>
2023-03-09 17:32:55 +0300

    09.03.2023

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -68,4 +68,13 @@ namespace Business.Constants
         public static string RentalsListed = "Kiralama işlemlerinin tümü listelendi";
         public static string RentalByIdListed = "Seçilen Kiralama işlemi listelendi";
     }
+    public static class CarImagesMessages
+    {
+        public static string CarImageAdded = "Araç'a resim eklendi";
+        public static string CarImageDeleted = "Araç'ın resmi silindi";
+        public static string CarImageUpdated = "Araç'ın resmi güncellendi";
+        public static string CarImagesListed = "Araç resimlerinin tümü listelendi";
+        public static string CarImageByIdListed = "Seçilen araç'ın resmi listelendi";
+        public static string CarImageLimitExceded = "Görsel limitini aştığınız için daha fazla görsel eklenemiyor.";
+    }
 }
