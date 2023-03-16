f72e715646efa1199ad3e141ca71151a4314cfda
Kadir Hali <kadirhali07@gmail.com>
2023-03-16 15:43:32 +0300

    16.03.2023

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -24,7 +24,7 @@ namespace Business.Concrete
         {
             _carDal = carDal;
         }
-
+        [SecuredOperation("admin,")]
         [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {    
--- Core/Entities/Concrete/User.cs
+++ Core/Entities/Concrete/User.cs
@@ -1,22 +1,19 @@
-﻿using Core.Entities;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Entities.Concrete
+namespace Core.Entities.Concrete
 {
     public class User : IEntity
     {
-        [Key]
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public int Password { get; set; }
-        public List<Customer> Customer { get; set; }
-
+        public byte[] PasswordSalt { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public bool Status { get; set; }
     }
 }
