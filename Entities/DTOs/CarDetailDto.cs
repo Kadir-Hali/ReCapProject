41b380f8c3520b256a53707557c91c1824641dde
Kadir Hali <kadirhali07@gmail.com>
2023-04-04 17:53:44 +0300

    04.04.2023

--- Entities/DTOs/CarDetailDto.cs
+++ Entities/DTOs/CarDetailDto.cs
@@ -4,8 +4,12 @@ namespace Entities.DTOs;
 
 public class CarDetailDto : IDto
 {
-    public string Description { get; set; }
+    public int Id { get; set; }
+    public string CarName { get; set; }
     public string BrandName { get; set; }
     public string ColorName { get; set; }
     public decimal DailyPrice { get; set; }
+    public string ModelYear { get; set; }
+    public string Description { get; set; }
+    public List<string> CarImage { get; set; }
 }
--- Entities/DTOs/RentalDetailDto.cs
+++ Entities/DTOs/RentalDetailDto.cs
@@ -4,7 +4,8 @@ namespace Entities.DTOs;
 
 public class RentalDetailDto : IDto
 {
-    public string Description { get; set; }
+    public int Id { get; set; }
+    public string CarName { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public DateTime RentDate { get; set; }
